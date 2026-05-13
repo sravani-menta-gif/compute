02e98584955fe11df07c129085eb032b76021579
Sravani <sravanim0308@gmail.com>
2026-05-13 12:56:13 +0530

    fix: Updates AMI ID

--- main.tf
+++ main.tf
@@ -57,9 +57,25 @@ resource "aws_security_group" "ec2_sg" {
   }
 }
 
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 # EC2 Instance
 resource "aws_instance" "web_server" {
-  ami                    = "ami-00118d76371cb02ee"
+  ami                    = data.aws_ami.amazon_linux.id
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
